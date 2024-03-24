7e37f7acd5a53fc4459786b69df2214a4802e3c8
xiexi1990 <xiexi_1990@126.com>
2024-03-24 05:44:42 +0000

    fix run_ddp.bash

--- run_ddp.bash
+++ run_ddp.bash
@@ -76,11 +76,11 @@ for i in "${!slave_ips[@]}"; do
   echo "Running on slave node $((i+1)) (IP: $ip)..."
 
   # Prepare commands for updating project directory and copying Python files
-  update_cmd="cd $proj_root; git pull"
-  copy_cmd="echo 'Copying Python files from master...'; scp $master_ip:$proj_root/*.py $proj_root/"
+  update_cmd="cd $proj_root && git pull"
+  copy_cmd="echo 'Copying Python files from master...' && scp $master_ip:$proj_root/*.py $proj_root/"
 
   # Command to run Python script on slave node, executed in the background
-  slave_command="($update_cmd) && ($copy_cmd) && (python $python_script $args --log_root $log_root --master_ip $master_ip --world_size $world_size --node_rank_begin $rank_begin &)"
+  slave_command="$update_cmd && $copy_cmd && python $python_script $args --log_root $log_root --master_ip $master_ip --world_size $world_size --node_rank_begin $rank_begin &"
 
   # Execute the commands on the slave node
   ssh aiscuser@$ip "$slave_command"  # Execute the commands on the slave node
