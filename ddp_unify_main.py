e1d17a4533ab0f3ddfd13590b6ea1a0c6866261c
xiexi51 <xiexi_1990@126.com>
2025-09-08 06:34:34 +0000

    update vit dropout 2

--- ddp_unify_main.py
+++ ddp_unify_main.py
@@ -147,7 +147,9 @@ def process(pn, args):
 
     elif args.v_type == "vit": 
         vit_name = args.vit_model 
-        model = timm.create_model(vit_name, pretrained=False, num_classes=num_classes, drop_rate=args.vit_drop_rate, attn_drop_rate=args.vit_attn_drop_rate, drop_path_rate=args.vit_drop_path_rate)
+        model = timm.create_model(vit_name, pretrained=False, num_classes=num_classes, drop_rate=args.vit_drop_rate, 
+                                  attn_drop_rate=args.vit_attn_drop_rate, drop_path_rate=args.vit_drop_path_rate, 
+                                  pos_drop_rate=args.vit_pos_drop_rate, proj_drop_rate=args.vit_proj_drop_rate)
         if args.vit_attn_type != 'original':
             model = replace_vit_attention(model, attn_type=args.vit_attn_type, pos_type=args.vit_pos_type, use_psd_square=args.vit_use_psd_square, 
                                           c0=args.vit_quad_c[0], c1=args.vit_quad_c[1], c2=args.vit_quad_c[2])
@@ -638,7 +640,9 @@ if __name__ == "__main__":
     parser.add_argument('--vit_attn_type', type=str, choices=['poly_kernel', 'quad_kernel', 'pos_kernel', 'original'])
 
     # Common
-    parser.add_argument("--vit_drop_rate", type=float, default=0.1, help="Dropout rate for MLP / proj / pos / head")
+    parser.add_argument("--vit_drop_rate", type=float, default=0.1)
+    parser.add_argument("--vit_pos_drop_rate", type=float, default=0.1)
+    parser.add_argument("--vit_proj_drop_rate", type=float, default=0.1)
     parser.add_argument("--vit_attn_drop_rate", type=float, default=0.1, help="Dropout rate for attention weights (attn_drop)")
     parser.add_argument("--vit_drop_path_rate", type=float, default=0.2, help="Stochastic depth (DropPath) rate")
 
